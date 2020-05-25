8fed86d973404dc3a9612620183bde76b748126d
Morgan C Nicholson <nich.morgan@outlook.com>
2020-05-25 18:16:40 -0300

    Adicionar arquivos de projeto.

---
+++ TodoApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseNpgsql("Host=localhost;Database=marketplace_acessiblidade_development;Username=postgres;Password=postgres")
+                .UseSnakeCaseNamingConvention();
+    }
+}
